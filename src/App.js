ec8d6b25c101593a28b8e13c7edb5e45c77984ef
Mez Dek <mezdekosman@hotmail.com>
2021-11-23 22:49:54 +0100

    finished challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import QuoteCard from "./components/QuoteCard";
+import axios from "axios";
 
 function App() {
+  const [quote, setQuote] = useState([]);
+
+  const getQuote = () => {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then((data) => {
+        setQuote(data.data);
+      })
+      .catch((err) => console.log("Error: ", err));
+  };
+
+  useEffect(getQuote, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {quote.map((item, index) => (
+        <QuoteCard key={index} {...item} />
+      ))}
+      <button onClick={getQuote}>Get another quote</button>
     </div>
   );
 }
